71700add2d4a5dff949484fc69435abf6788b002
JustinSheng <justin.wang4@icloud.com>
2024-03-31 11:55:19 +0800

    Hashable

--- types_and_operators.playground/Pages/Hashable.xcplaygroundpage/Contents.swift
+++ types_and_operators.playground/Pages/Hashable.xcplaygroundpage/Contents.swift
@@ -2,6 +2,40 @@
 
 import Foundation
 
+struct Email: Hashable {
+	init?(_ raw: String) {
+		guard raw.contains("@") else {
+			return nil
+		}
+		address = raw
+	}
+	
+	private(set) var address: String
+}
+
+class User: Hashable {
+	var id: Int?
+	var name: String
+	var email: Email
+	
+	init(id: Int?, name: String, email: Email) {
+		self.id = id
+		self.name = name
+		self.email = email
+	}
+	
+	static func ==(lhs: User, rhs: User) -> Bool {
+		return lhs.id == rhs.id &&
+		lhs.name == rhs.name &&
+		lhs.email == rhs.email
+	}
+	
+	func hash(into hasher: inout Hasher) {
+		hasher.combine(id)
+		hasher.combine(name)
+		hasher.combine(email)
+	}
+}
 
 
 //: [Next](@next)
