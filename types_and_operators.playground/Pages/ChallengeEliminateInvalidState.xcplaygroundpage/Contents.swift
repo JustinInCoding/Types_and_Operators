087541fbbe88c3dd074c765a54f8863f82efa0c8
JustinSheng <justin.wang4@icloud.com>
2024-03-31 12:41:19 +0800

    Challenge: Eliminate Invalid State

--- types_and_operators.playground/Pages/ChallengeEliminateInvalidState.xcplaygroundpage/Contents.swift
+++ types_and_operators.playground/Pages/ChallengeEliminateInvalidState.xcplaygroundpage/Contents.swift
@@ -1,7 +1,56 @@
 //: [Previous](@previous)
+// Copyright (c) 2019 Razeware LLC
+// See Copyright Notice page for details about the license.
 
 import Foundation
+import struct CoreGraphics.CGFloat
+import struct CoreGraphics.CGPoint
+import struct CoreGraphics.CGSize
+import struct CoreGraphics.CGRect
 
-var greeting = "Hello, playground"
+// Refactor Shape so that it is self documenting and makes it impossible to
+// represent invalid states.
+
+struct CGAngle {
+	var radians: CGFloat
+}
+
+extension CGAngle {
+	@inlinable init(degrees: CGFloat) {
+		radians = degrees / 180.0 * CGFloat.pi
+	}
+	
+	@inlinable var degrees: CGFloat {
+		get {
+			return radians / CGFloat.pi * 180
+		}
+		set {
+			radians = newValue / 180.0
+		}
+	}
+}
+
+extension CGAngle: CustomStringConvertible {
+	var description: String {
+		return String(format: "%0.2f°", degrees)
+	}
+}
+
+let angle = CGAngle(radians: .pi)
+
+enum Shape {
+	case circle(center: CGPoint, radius: CGFloat)
+	case square(origin: CGPoint, size: CGFloat)
+	case rotatedSquare(origin: CGPoint, size: CGFloat, angle: CGAngle)
+	case rect(CGRect)
+	case rotatedRect(CGRect, CGAngle)
+	case ellipse(CGRect)
+	case rotatedEllipse(CGRect, CGAngle)
+}
+
+// Make a circle with a center at the origin and radius of 10
+
+let radius: CGFloat = 10
+let circle = Shape.circle(center: .zero, radius: radius)
 
 //: [Next](@next)
