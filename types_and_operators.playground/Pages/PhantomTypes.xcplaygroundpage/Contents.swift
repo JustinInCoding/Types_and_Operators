de586186d8f792fd0f1abe0e167f3ab844a0156f
JustinSheng <justin.wang4@icloud.com>
2024-03-31 12:17:53 +0800

    Phantom Types

--- types_and_operators.playground/Pages/PhantomTypes.xcplaygroundpage/Contents.swift
+++ types_and_operators.playground/Pages/PhantomTypes.xcplaygroundpage/Contents.swift
@@ -1,7 +1,45 @@
 //: [Previous](@previous)
-
+// 虚类型
 import Foundation
 
-var greeting = "Hello, playground"
+//typealias State = String
+//typealias Capital = String
+
+struct Named<T>: Hashable, ExpressibleByStringLiteral, CustomStringConvertible {
+	var name: String
+	init(_ name: String) {
+		self.name = name
+	}
+	init(stringLiteral value: String) {
+		name = value
+	}
+	var description: String {
+		return name
+	}
+}
+
+enum StateTag {}
+enum CapitalTag {}
+
+typealias State = Named<StateTag>
+typealias Capital = Named<CapitalTag>
+
+var lookup: [State: Capital] = ["Alabama": "Montgomery",
+																"Alaska":  "Juneau",
+																"Arizona": "Phoenix"]
+
+func printStateAndCapital(_ state: State, _ capital: Capital) {
+	print("The capital of \(state) is \(capital)")
+}
+
+func test() {
+	let alaska = State("Alaska")
+	guard let capital = lookup[alaska] else {
+		return
+	}
+//	printStateAndCapital(capital, alaska)
+	printStateAndCapital(alaska, capital)
+}
+test()
 
 //: [Next](@next)
